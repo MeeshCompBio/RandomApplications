9f3869a06a9998c09b4dc8cedb27c7a77b433599
MeeshCompBio <jmichno@DESKTOP-LT123LC.localdomain>
2020-10-30 22:48:20 -0500

    added form params

--- javascript/fibonacci/client/src/Fib.js
+++ javascript/fibonacci/client/src/Fib.js
@@ -26,15 +26,52 @@ class Fib extends Component {
         })
     }
 
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        await axios.post('/api/values', {
+            index: this.state.index
+        });
+    };
+
+    renderSeenIndexes() {
+        return this.state.seenIndexes.map(({ number}) => number).join(', ');
+    }
+
+    renderValues() {
+        const entries = [];
+
+        for (let key in this.state.values) {
+            entries.push(
+                <div key={key}>
+                    For index {key} we calculated {this.state.values[key]}
+                </div>
+            );
+        }
+
+        return entries;
+    }
+    
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Enter your index</label>
-                    <input />
+                    <input 
+                        value={this.state.index}
+                        onChange={event => this.setState({index: event.target.value})}
+                    />
                     <button>Get your Fib</button>
                 </form>
+
+                <h3> Indexs used in the past</h3>
+                {this.renderSeenIndexes()}
+
+                <h3>Calculated Values:</h3>
+                {this.renderValues()}
             </div>
-        )
+        );
     }
 }
+
+export default Fib;
